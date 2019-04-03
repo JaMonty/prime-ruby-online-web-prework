8809630e4d41a5d80cb27ac83777a817a1090c61
Jason  Montes <jasone.montes@gmail.com>
2019-04-03 06:45:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,10 @@
 # Add  code here!
-
+describe "prime?" do
+  it 'returns true for prime numbers' do
+    expect(prime?(2)).to be(true)
+    expect(prime?(3)).to be(true)
+    expect(prime?(11)).to be(true)
+    expect(prime?(105557)).to be(true)
+  end
+  
+  
